87aa0ca9257b84ebe9e47c4646bb302597358dd0
Sandra Tello <stello.salinas@gmail.com>
2024-03-11 17:07:35 -0600

    Add bootstrap files, improve templates and add the codes to the home template

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
 @app.route('/your-url', methods=['GET','POST'])
 def your_url():
@@ -27,7 +27,7 @@ def your_url():
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('/Users/sandratello/Desktop/FlaskEssentialTraining/' + full_name)
+            f.save('/Users/sandratello/Desktop/FlaskEssentialTraining/static/user_files/' + full_name)
             urls[request.form['code']] = {'file':full_name}
 
         with open('urls.json','w') as url_file:
