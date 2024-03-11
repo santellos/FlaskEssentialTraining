a78d942d1e5c5d5543c2139e82908ab0b03c8153
Sandra Tello <stello.salinas@gmail.com>
2024-03-11 16:23:32 -0600

    Add page not found template and route

--- app.py
+++ app.py
@@ -46,3 +46,8 @@ def redirect_to_url(code):
                     return redirect(urls[code]['url']) 
                 else:
                     return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
