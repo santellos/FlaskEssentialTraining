2112f4a3488bd77f22d311528a5d5c5bbb24e0de
Sandra Tello <stello.salinas@gmail.com>
2024-03-11 15:16:25 -0600

    Create home template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Flask!'
+    return render_template('home.html', name='Nick')
 
 @app.route('/about')
 def about():
