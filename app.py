5385859779f0c83fa767cdb67d9d534fffed4bfd
Sandra Tello <stello.salinas@gmail.com>
2024-03-11 15:24:20 -0600

    Add form to template

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='Nick')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
