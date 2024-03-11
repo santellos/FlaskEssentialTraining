b4dd4efd8a0df0ee25fa2de0c57982b9499c98d6
Sandra Tello <stello.salinas@gmail.com>
2024-03-11 17:44:14 -0600

    Add blueprints and views for organization

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify, Blueprint
 import json
 import os.path
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
-app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
+bp = Blueprint('urlshort',__name__)
 
-@app.route('/')
+
+@bp.route('/')
 def home():
     return render_template('home.html', codes=session.keys())
 
-@app.route('/your-url', methods=['GET','POST'])
+@bp.route('/your-url', methods=['GET','POST'])
 def your_url():
     if request.method == 'POST':
         urls = {}
@@ -37,7 +37,7 @@ def your_url():
     else:
         return redirect(url_for('home'))
     
-@app.route('/<string:code>')
+@bp.route('/<string:code>')
 def redirect_to_url(code):
     if os.path.exists('urls.json'):
         with open('urls.json') as urls_file:
@@ -49,10 +49,10 @@ def redirect_to_url(code):
                     return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
     return abort(404)
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
-@app.route('/api')
+@bp.route('/api')
 def session_api():
     return jsonify(list(session.keys()))
