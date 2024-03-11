931efd9848769431bb37ce505ae211d533088f3c
Sandra Tello <stello.salinas@gmail.com>
2024-03-11 16:12:04 -0600

    Add the upload file feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
@@ -21,7 +22,13 @@ def your_url():
             flash('That short name has already been taken. Please select another name.')
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url':request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url':request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/Users/sandratello/Desktop/url-shortener/' + full_name)
+
         with open('urls.json','w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
